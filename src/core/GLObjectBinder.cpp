222b6e7e563b0ba81a8cb3d7af29d48702df4829
IohannesFolbort <petrarce@gmail.com>
2022-04-27 23:32:13 +0200

    fix VertexArray linkage to ElementArrayBuffer

--- src/core/GLObjectBinder.cpp
+++ src/core/GLObjectBinder.cpp
@@ -6,14 +6,12 @@ namespace core {
 GlObjectBinder::GlObjectBinder(GLObject& object)
 	: mGlObject(object)
 {
-	std::cerr << "Bind " << mGlObject.mName << std::endl;
 	mGlObject.bind();
 }
 
 GlObjectBinder::~GlObjectBinder()
 {
 	mGlObject.unbind();
-	std::cerr << "Unbind " << mGlObject.mName << std::endl;
 	
 }
 
