5118e360eb5220d9dc4f26558c808973a4574850
Iohannes <iohannes.folbort@indurad.com>
2022-04-29 16:19:14 +0200

    add gl shaders

---
+++ src/core/GLProgram.cpp
@@ -0,0 +1,28 @@
+#include <GLProgram.hpp>
+namespace glwrapper {
+
+namespace core {
+
+GLProgram::GLProgram(const std::string& name)
+		:GLObject(name) 
+{
+	mObjectId = glCreateProgram();
+};
+GLProgram::~GLProgram()
+{
+	glDeleteProgram(mObjectId);
+}
+	
+void GLProgram::bind()
+{
+	glGetIntegerv(GL_ACTIVE_PROGRAM, &mObjectBeforeBinding);
+	glUseProgram(mObjectId);
+}
+
+void GLProgram::unbind()
+{
+	glUseProgram(mObjectBeforeBinding);
+}
+
+}
+}
