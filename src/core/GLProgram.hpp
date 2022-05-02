9259ebce11f61114a6f427718d5d150fc0d90e5e
petrarce <petrarce@gmail.com>
2022-05-02 09:33:27 +0200

    Add uniforms to program

--- src/core/GLProgram.hpp
+++ src/core/GLProgram.hpp
@@ -43,6 +43,30 @@ public:
 		return "Success";
 	}
 
+#define UNIFORM_SETTER(typeSuffix, type) \
+	template<class... VarueArgs> \
+	void setUniform(const std::string& var, type v1, const VarueArgs&... val) \
+	{ \
+		int components = sizeof...(val) + 1; \
+		GLint location = glGetUniformLocation(mObjectId, var.c_str()); \
+	 \
+		if(components == 1) \
+			glUniform1##typeSuffix(location, v1, val...); \
+		else if(components == 2) \
+			glUniform1##typeSuffix(location, v1, val...); \
+		else if(components == 3) \
+			glUniform1##typeSuffix(location, v1, val...); \
+		else if(components == 4) \
+			glUniform1##typeSuffix(location, v1, val...); \
+		else \
+			throw std::runtime_error("Failed to set uniform with more than 4 elements"); \
+	}
+
+
+	UNIFORM_SETTER(f, float)
+	UNIFORM_SETTER(i, int)
+	UNIFORM_SETTER(ui, unsigned int)
+
 	void use() { glUseProgram(mObjectId); }
 
 protected:
