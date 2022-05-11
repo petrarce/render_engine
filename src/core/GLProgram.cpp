4f3bc3a36edaff816e0e3ff8b1796d268783a864
petrarce <petrarce@gmail.com>
2022-05-11 10:09:43 +0200

    Use debug glad and enable throw callback

--- src/core/GLProgram.cpp
+++ src/core/GLProgram.cpp
@@ -7,26 +7,21 @@ GLProgram::GLProgram(const std::string &name)
 	: GLObject(name)
 {
 	mObjectId = glCreateProgram();
-	GL_THROW_ON_ERROR();
 };
 GLProgram::~GLProgram()
 {
 	glDeleteProgram(mObjectId);
-	GL_THROW_ON_ERROR();
 }
 
 void GLProgram::bind()
 {
 	glGetIntegerv(GL_CURRENT_PROGRAM, &mObjectBeforeBinding);
-	GL_THROW_ON_ERROR();
 	glUseProgram(mObjectId);
-	GL_THROW_ON_ERROR();
 }
 
 void GLProgram::unbind()
 {
 	glUseProgram(mObjectBeforeBinding);
-	GL_THROW_ON_ERROR();
 }
 
 } // namespace core
