e976131d294dffb8f6359c8cce9a6b098f4ed622
petrarce <petrarce@gmail.com>
2022-05-12 18:57:04 +0200

    Add uniform matrix setter interface

--- src/core/GLProgram.hpp
+++ src/core/GLProgram.hpp
@@ -5,6 +5,10 @@
 #include <GLObject.hpp>
 #include <GLObjectBinder.hpp>
 #include <GLShader.hpp>
+#include <Eigen/Dense>
+
+#include <vector>
+#include <utility>
 
 namespace glwrapper
 {
@@ -67,6 +71,37 @@ public:
 	UNIFORM_SETTER_API(f, float)
 	UNIFORM_SETTER_API(i, int)
 	UNIFORM_SETTER_API(ui, unsigned int)
+
+	template <int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+	void setMatrix(
+		const std::string &name,
+		Eigen::Matrix<float, _Rows, _Cols, _Options, _MaxRows, _MaxCols> &mat,
+		bool transpose = false)
+	{
+		typedef void (*GlMatrixSetter)(GLint location, GLsizei count,
+									   GLboolean transpose,
+									   const GLfloat *value);
+		if (mat.cols() > 4 || mat.rows() > 4 || mat.rows() < 2 ||
+			mat.cols() < 2)
+			throw std::runtime_error("invalid matrix size");
+
+		static std::map<std::pair<int, int>, GlMatrixSetter> functions = {
+			{std::make_pair(2, 2), glUniformMatrix2fv},
+			{std::make_pair(2, 3), glUniformMatrix2x3fv},
+			{std::make_pair(2, 4), glUniformMatrix2x4fv},
+
+			{std::make_pair(3, 2), glUniformMatrix3x2fv},
+			{std::make_pair(3, 3), glUniformMatrix3fv},
+			{std::make_pair(3, 4), glUniformMatrix3x4fv},
+
+			{std::make_pair(4, 2), glUniformMatrix4x2fv},
+			{std::make_pair(4, 3), glUniformMatrix4x3fv},
+			{std::make_pair(4, 4), glUniformMatrix4fv},
+		};
+		GLint location = glGetUniformLocation(mObjectId, name.c_str());
+		functions.find(std::make_pair(mat.rows(), mat.cols()))
+			->second(location, 1, transpose, mat.data());
+	}
 
 protected:
 	bool link()
