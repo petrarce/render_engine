c3e7a67df0b17875fd4ea510835d40e1866491cb
Iohannes <iohannes.folbort@indurad.com>
2022-05-13 18:15:39 +0200

    Modify tests for camera controller

--- src/objects/camera/Camera.hpp
+++ src/objects/camera/Camera.hpp
@@ -8,23 +8,25 @@ namespace camera
 class GLCamera
 {
 public:
-	Eigen::Vector3f pivot = Eigen::Vector3f(0, 0, 0);
-	float angleX{0};
-	float angleZ{0};
-	float distance{10};
-	const Eigen::Vector3d upAxis = Eigen::Vector3f(0, 1, 0);
-	const Eigen::Vector3d rightAxis = Eigen::Vector3f(1, 0, 0);
+	GLCamera &operator=(const GLCamera &other) = default;
 
 	GLCamera(const Eigen::Vector3f &ua = Eigen::Vector3f(0, 1, 0),
 			 const Eigen::Vector3f &ra = Eigen::Vector3f(1, 0, 0))
 		: upAxis(ua.normalized())
-		, rightAxis(ra.cross(ua).cross(ra).normalized())
+		, rightAxis(ua.cross(ra.cross(ua)).normalized())
 	{
 	}
 	Eigen::Matrix4f transform() const;
 	Eigen::Matrix4f toViewTransform() const;
+	Eigen::Matrix3f rotation() const;
+	Eigen::Vector3f translation() const;
 
-private:
+	Eigen::Vector3f pivot = Eigen::Vector3f(0, 0, 0);
+	float angleX{0};
+	float angleZ{0};
+	float distance{10};
+	Eigen::Vector3f upAxis = Eigen::Vector3f(0, 1, 0);
+	Eigen::Vector3f rightAxis = Eigen::Vector3f(1, 0, 0);
 };
 
 } // namespace camera
--- src/objects/camera/CameraController.hpp
+++ src/objects/camera/CameraController.hpp
@@ -9,30 +9,9 @@ namespace camera
 class GLCameraController
 {
 public:
-	void cameraMove(const Eigen::Vector3f &camSpaceOffset)
-	{
-		Eigen::Vector4f homogVect;
-		homogVect << camSpaceOffset, 1.f;
-		Eigen::Vector3f worldSpaceOffset =
-			camera.transform().block<3, 3>(0, 0) * camSpaceOffset;
-		camera.pivot += worldSpaceOffset;
-		std::cout << "Camera moved to " << camera.pivot << std::endl;
-	}
+	void cameraMove(const Eigen::Vector3f &camSpaceOffset);
 
-	void cameraRotate(float angleXOffset, float andleZOffset)
-	{
-		camera.angleX += angleXOffset;
-		camera.angleZ += andleZOffset;
-	}
-
-	void screenSpaceMove(float dx, float dy)
-	{
-		cameraMove(Eigen::Vector3f(dx, dy, 0));
-	}
-	void screenSpaceRotate(float dx, float dy)
-	{
-		cameraRotate(dy, dx);
-	}
+	void cameraRotate(float angleXOffset, float andleZOffset);
 
 	GLCamera camera;
 };
