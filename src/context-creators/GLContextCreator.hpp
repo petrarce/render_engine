577708af22b0fa00ac932f975b5609c4b1b9f8e8
petrarce <petrarce@gmail.com>
2022-05-15 00:01:30 +0200

    Fix camera and add first demo

---
+++ src/context-creators/GLContextCreator.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <string>
+namespace glcontext
+{
+namespace creators
+{
+class GLContextCreator
+{
+public:
+	friend class GLContextBinder;
+
+	GLContextCreator(const std::string &name = "GLContextCreator")
+	{
+	}
+	virtual ~GLContextCreator()
+	{
+	}
+	GLContextCreator(const GLContextCreator &) = delete;
+	GLContextCreator &operator=(const GLContextCreator &) = delete;
+	GLContextCreator(GLContextCreator &&) = delete;
+	GLContextCreator &operator=(GLContextCreator &&) = delete;
+
+	std::string mName;
+
+protected:
+	virtual void createContext() = 0;
+	virtual void destroyContext() = 0;
+
+private:
+};
+
+class GLContextBinder
+{
+public:
+	GLContextBinder(GLContextCreator &context)
+		: mContextCreator(context)
+	{
+		mContextCreator.createContext();
+	}
+	~GLContextBinder()
+	{
+		mContextCreator.destroyContext();
+	}
+
+	GLContextBinder() = delete;
+	GLContextBinder(const GLContextBinder &) = delete;
+	GLContextBinder &operator=(const GLContextBinder &) = delete;
+	GLContextBinder(GLContextBinder &&) = delete;
+	GLContextBinder &operator=(GLContextBinder &&) = delete;
+
+private:
+	GLContextCreator &mContextCreator;
+};
+
+} // namespace creators
+} // namespace glcontext
